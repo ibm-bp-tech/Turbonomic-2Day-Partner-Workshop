71c77041225345aeb51ca4730fa874bcc68e3342
Eyad Bustami <bustami@us.ibm.com>
2023-01-30 21:24:27 -0800

    updates done by Eyad

--- src/gatsby-theme-carbon/components/LeftNav/ResourceLinks - Copy (2).js
+++ src/gatsby-theme-carbon/components/LeftNav/ResourceLinks - Copy (2).js
@@ -15,13 +15,9 @@ const links = [
     title: 'IBM AIOps Site',
     href: 'https://www.ibm.com/cloud/aiops',
   },
- {
-    title: 'PTEG',
-    href: 'https://ibm.seismic.com/Link/Content/DCGdFfFmRb3Rb8TRTHhHPXjbCDWj',
-   },
   {
-    title: 'PTEG - Turbonomic',
-    href: 'https://ibm.seismic.com/Link/Content/DCGVQ3p7FQbGj8CC9GgP32HP6GPj',
+    title: 'PTEG - Partner Enablement',
+    href: 'https://ibm.seismic.com/Link/Content/DCGdFfFmRb3Rb8TRTHhHPXjbCDWj',
   },
 ];
 
